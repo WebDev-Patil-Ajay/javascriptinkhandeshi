e647514f30ddc8904c3328efededc6a961d784d4
WebDev-Patil-Ajay <patilaajay452@gmail.com>
2025-02-22 09:12:35 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,15 @@
+// const a=50
+// const b=60
+// var c=1
+// console.log(c)
+//  c=a+b;
+ 
+// console.log(a , b ,"=",c)
+
+
+let n=12
+let age=56
+c=n+age
+console.log(typeof c)
+console.log(typeof age)
+console.log(typeof n)
