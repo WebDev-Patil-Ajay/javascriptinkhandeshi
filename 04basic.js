e2110ade3c09250e3d920b47cef8fec6a0dcd322
Ajay Patil <patilaajay452@gmail.com>
2025-02-24 08:45:31 +0530

    Add files via upload

---
+++ 04basic.js
@@ -0,0 +1,30 @@
+// *********************** string ***********************
+
+
+const name = 'Ajay Patil'
+const game = 'Cricket'
+const gameName = 'pubg-free-fire-cod'
+console.log(name.length)   
+console.log(name.toUpperCase())
+console.log(name.toLowerCase()) 
+console.log(name.slice(0, 4))
+console.log(name.slice(-4))
+console.log(name.charAt(2))
+console.log(name.indexOf('j'))
+console.log(name.lastIndexOf('a'))
+console.log(name.endsWith('l'))
+console.log(name.startsWith('o'))
+console.log(name.split(','))
+console.log(name.replace('ajay', 'harshal'))
+console.log(name.includes('j'))
+console.log(name.concat(' ', game))
+console.log(gameName.split(','))
+console.log(name.repeat())
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('hitesh%20choudhary', 'patil@Ajay'))
+
+// console.log(url.includes('com'))
+
+// console.log(gameName.split('-'));
