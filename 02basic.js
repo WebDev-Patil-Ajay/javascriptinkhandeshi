bb730f8821d46fc159fed839ce392eaddaf59d0a
Ajay Patil <patilaajay452@gmail.com>
2025-02-24 08:40:56 +0530

    Add files via upload

---
+++ 02basic.js
@@ -0,0 +1,40 @@
+// *********************** Operations ***********************
+
+let value =3
+let posValue = -3
+console.log(posValue)
+
+console.log("Addition =",2+2);
+console.log("subtraction =",2-2);
+console.log("multiplication =",2*2);
+console.log("power =",2**3);
+console.log("division =",2/3);
+console.log("modulas =",2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+console.log(num1)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
